60aa494fccbb02cca3dc32860abcd0b333995545
Igor Santana <igorsantana@igorpsant.local>
2015-07-05 20:19:45 -0300

    Arrumado bug de importação de arquivo

--- openFile.js
+++ openFile.js
@@ -1,31 +1,26 @@
-var fs = require('fs'),	endOfLine = require('os').EOL,tgf,_break,listOfAdjacency= [];
-fs.readFile('./graph.tgf',{encoding: 'utf-8'},function(err,data){
-	if(err) throw err;
-	readData(data)
-});
-function readData(file){
-	var index,indexToBreak,edges = [];
-	// Quebra arquivo .tgf por linha
-	tgf = file.split(endOfLine);
-	// Pega o index do divisor de vértices # arestas
-	for(var i = 0, len = tgf.length;i < len; i++) if(tgf[i].indexOf('#') != -1){
-		indexToBreak = i;
-		break;
-	};
-	// Cria a lista de adjacência com o array de vértices inicializados com 0 vértices.
-	listOfAdjacency = tgf.splice(0,indexToBreak).map(function(vertex){
+var fs = require('fs'), endOfLine = require('os').EOL;
+
+module.exports = function(filename){
+	var fileContent = fs.readFileSync(filename, {encoding: 'utf-8'})
+	,	rawFileContent = fileContent.split(endOfLine)
+	,	indexToBreak = rawFileContent.indexOf('#')
+	,	listOfAdjacency
+	,	edges
+	,	from
+	,	to;
+	function mountListOfAdjacency(vertex){
 		var aux = vertex.split(' ');
-		return {value: aux[0],label: aux[1],edges: []}
-	});
-	// Criar o array com os elementos depois do #
-	edges = tgf.splice(1, tgf.length -1);
-	var from, to;
-	// Adiciona os vértices dentro da lista de vértices dos itens na lista de adjacências
-	edges.forEach(function(value){
-		from = parseInt(value.split(' ')[0]);
-		to = parseInt(value.split(' ')[1]);
-		listOfAdjacency[from-1].edges.push(listOfAdjacency[to-1].value)
-	})
-	
-	console.log(listOfAdjacency);
+		return {value: aux[0],label: aux[1],edges: []};
+	}
+	function finishListOfAdjacency(edges){
+		from = parseInt(edges.split(' ')[0]);
+		to = parseInt(edges.split(' ')[1]);
+		if(!(isNaN(from) || isNaN(to))){
+			listOfAdjacency[from -1].edges.push(listOfAdjacency[to-1].value);
+		}
+	}
+	listOfAdjacency = rawFileContent.splice(0, indexToBreak).map(mountListOfAdjacency);
+	edges = rawFileContent.splice(1, rawFileContent.length -1);
+	edges.forEach(finishListOfAdjacency);
+	return listOfAdjacency;
 }
